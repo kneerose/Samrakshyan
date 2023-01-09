4c1aaa4b18c464ffc11d0d41edc43106151cd0c0
Niraj <duwalkneerose@gmail.com>
2023-01-09 07:29:12 +0545

    Fix cors issue

--- src/store/detail/api.ts
+++ src/store/detail/api.ts
@@ -6,14 +6,14 @@ export const responseApi = createApi({
   reducerPath: PREDICTION_RESPONSE_REDUCER_KEY,
   tagTypes: ["RESPONSE"], //autofetching
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://ec2-3-145-63-255.us-east-2.compute.amazonaws.com:8000",
+    baseUrl: "http://ec2-3-17-77-8.us-east-2.compute.amazonaws.com:8000",
   }),
   endpoints: (builder) => ({
     postPredictionResponse: builder.mutation<predictionDto, FormData>({
       query: (requestFileString) => ({
         url: "/files/",
         method: "POST",
-        credentials: "include",
+        credentials: "same-origin",
         body: requestFileString,
       }),
     }),
